31ab8542701f14b94352418de136430ba655312b
advjuniorpedrocolombo-arch <advjuniorpedrocolombo@gmail.com>
2026-09-14 19:52:29 -0300

    Registra banco central LTP na configuração do portal

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 window.PORTAL_CONFIG={
   apiUrl:"",
-  turma:"2MTEC-CONTABILIDADE-MAIRINQUE",
-  componente:"LTP"
+  turma:"2º MTEC - Contabilidade - Mairinque",
+  componente:"LTP",
+  spreadsheetId:"1nQUnqLeDb3QfI_klo_WM2mEx6Yq4K-nQyp1wjU89Ox8",
+  driveRootId:"12spWnn4ZsCz8kVg6T8aSW8lg0bDYCKxi"
 };
