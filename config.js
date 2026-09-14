af1b4b9a190f4ab13464271d22c1f81e9f9c9ebc
advjuniorpedrocolombo-arch <advjuniorpedrocolombo@gmail.com>
2026-09-14 19:56:26 -0300

    Conecta portal LTP ao Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PORTAL_CONFIG={
-  apiUrl:"",
+  apiUrl:"https://script.google.com/macros/s/AKfycbzgUKN86QDFtz6b5zinDJszFU6RkNyZ-h-YzaUbxbueiMH5KgvE1ZTvw2rxNJ6Ck_I/exec",
   turma:"2º MTEC - Contabilidade - Mairinque",
   componente:"LTP",
   spreadsheetId:"1nQUnqLeDb3QfI_klo_WM2mEx6Yq4K-nQyp1wjU89Ox8",
